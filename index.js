c5643da94cc8598475045814ef6afa0a869ecbd7
dwisign <dwisign.mail@gmail.com>
2024-09-14 16:21:48 +0700

    update js sore

---
+++ index.js
@@ -0,0 +1,19 @@
+var cars = "Avanza, Mobilio, Ayla, Brio, Jazz"
+console.log(cars)
+//document.writeln(cars)
+
+var gretting = "Hello World"
+console.log(gretting)
+//document.writeln(gretting)
+
+var dataString = "Ini string"
+var dataNumber = 1
+var dataBoolean = true //false
+var a = 20
+var b = 30
+var result = a*b
+
+console.log(dataString)
+console.log(dataNumber)
+console.log(dataBoolean)
+console.log(result)
