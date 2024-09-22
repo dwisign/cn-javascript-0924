32b41d1682f5786796a6b8e1a53a918e4823c1a7
dwisign <dwisign.mail@gmail.com>
2024-09-22 10:35:48 +0700

    update style & indicator color

--- bmiCalc.js
+++ bmiCalc.js
@@ -17,18 +17,27 @@ function bmiCalc(){
 
 
     //INDICATOR CATEGORY
+
+    const indicatorColor = document.getElementById('resultBox')
+
     if (bmiCalcCount < 0) {
         indicator = "Result not valid"
+        indicatorColor.classList.toggle('notValidColor', bmiCalcCount)
     } else if(bmiCalcCount >= 0 && bmiCalcCount <= 18.5){
         indicator = "Underweight"
+        indicatorColor.classList.toggle('underWeightColor', bmiCalcCount)
     } else if(bmiCalcCount >= 18.5 && bmiCalcCount <= 25.9){
         indicator = "Healthyweight"
+        indicatorColor.classList.toggle('healthyWeightColor', bmiCalcCount)
     } else if(bmiCalcCount >= 25.9 && bmiCalcCount <= 29.9){
         indicator = "Overweight"
+        indicatorColor.classList.toggle('overWeightColor', bmiCalcCount)
     } else if(bmiCalcCount >= 29.9 && bmiCalcCount <= 40) {
         indicator = "Obesity"
+        indicatorColor.classList.toggle('obesityColor',bmiCalcCount)
     } else{
         indicator = "Severe Obesity" 
+        indicatorColor.classList.toggle('serveObesityColor', bmiCalcCount)
     }
 
     //console.log(indicator)
